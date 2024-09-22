98ecf8f9eb043cd486dc0cc0c78477a9f56e2283
Mert Yılmaz <merttylmz_@outlook.com>
2024-09-22 21:06:24 +0300

    Initial Commit

---
+++ LittleLemonFinalProject/LittleLemonFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonFinalProjectApp.swift
+//  LittleLemonFinalProject
+//
+//  Created by Mert Yılmaz on 22.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
